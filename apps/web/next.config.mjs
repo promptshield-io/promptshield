88eaf771ba86f27acd19b704b8d8965e4f913fcc
Mayank <mayank.srmu@gmail.com>
2026-02-28 08:23:57 +0530

    chore(@app/web): :wrench: add favicon and logo

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -3,8 +3,6 @@ import { createMDX } from "fumadocs-mdx/next";
 /** Detect GitHub Actions */
 const isGitHubActions = process.env.GITHUB_ACTIONS === "true";
 
-console.log({ isGitHubActions });
-
 const withMDX = createMDX();
 
 /** @type {import('next').NextConfig} */
--- apps/web/src/lib/layout.shared.tsx
+++ apps/web/src/lib/layout.shared.tsx
@@ -1,9 +1,16 @@
 import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
+import Image from "next/image";
+import Link from "next/link";
+import icon from "../app/icon.png";
 
 export function baseOptions(): BaseLayoutProps {
   return {
     nav: {
-      title: "Turbo Forge",
+      title: (
+        <Link href="/" className="font-bold flex gap-2 items-center">
+          <Image src={icon} alt="PromptShield logo" width={24} /> PromptShield
+        </Link>
+      ),
     },
     githubUrl: "https://github.com/promptshield-io/promptshield",
   };
